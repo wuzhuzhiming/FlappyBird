46012f4891b37f578c194a3487136406d259c607
wuzhuzhiming <ithename@163.com>
2018-06-20 22:42:44 +0800

    添加背景移动

---
+++ Assets/SCripts/MapMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMove : MonoBehaviour
+{
+    public float speed = 200f;
+
+    // 如果 MonoBehaviour 已启用，则在每一帧都调用 Update
+    private void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if ((transform as RectTransform).anchoredPosition.x <= -767)
+        {
+            (transform as RectTransform).anchoredPosition = new Vector2(767f, 0);
+        }
+    }
+}
